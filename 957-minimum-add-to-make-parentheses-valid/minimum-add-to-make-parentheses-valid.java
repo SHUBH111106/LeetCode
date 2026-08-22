1ab233b2e7272a73a54118a6309d223d98f2f0f7
SHUBH111106 <gargshubh061@gmail.com>
2026-08-22 20:07:39 +0530

    Time: 1 ms (67.06%) | Memory: 43.1 MB (8.44%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int add = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                open++;
+            } 
+            else {
+                if (open > 0) {
+                    open--;
+                } 
+                else {
+                    add++;
+                }
+            }
+        }
+        return add + open;
+    }
+}
