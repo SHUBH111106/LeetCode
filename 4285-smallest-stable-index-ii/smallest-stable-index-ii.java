33386e8360712a988afec619c060ba9b63717f87
SHUBH111106 <gargshubh061@gmail.com>
2026-09-05 09:57:09 +0530

    Time: 3 ms (100.00%) | Memory: 133.1 MB (32.44%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -1,28 +1,22 @@
-class Solution {
-    public int firstStableIndex(int[] nums, int k) {
-
+class Solution 
+{
+    public int firstStableIndex(int[] nums, int k) 
+    {
         int n = nums.length;
-
-        int[] max = new int[n];
         int[] min = new int[n];
 
-        max[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            max[i] = Math.max(max[i - 1], nums[i]);
+        int mint = Integer.MAX_VALUE;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if (nums[i] < mint) mint = nums[i];
+            min[i] = mint;
         }
 
-        min[n - 1] = nums[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) {
-            min[i] = Math.min(min[i + 1], nums[i]);
-        }
-
-        for (int i = 0; i < n; i++) {
-
-            if (max[i] - min[i] <= k) {
-                return i;
-            }
+        int maxt = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] > maxt) maxt = nums[i];
+            if(maxt - min[i] <= k) return i;
         }
 
         return -1;
