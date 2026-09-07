7f9b1346b03a4d4d3f5cec3518ba74b3a825bcd9
SHUBH111106 <gargshubh061@gmail.com>
2026-09-07 22:46:42 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (14.30%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] ans = new int[n];
+        if (k == 0) return ans;
+        int sum = 0;
+        if (k > 0) {
+            // First window: elements after index 0
+            for (int i = 1; i <= k; i++) {
+                sum += code[i % n];
+            }
+
+            for (int i = 0; i < n; i++) {
+
+                ans[i] = sum;
+
+                // Remove first element of current window
+                sum -= code[(i + 1) % n];
+
+                // Add next element
+                sum += code[(i + k + 1) % n];
+            }
+
+        } else {
+
+            k = -k;
+
+            // First window: elements before index 0
+            for (int i = 1; i <= k; i++) {
+                sum += code[(n - i) % n];
+            }
+
+            for (int i = 0; i < n; i++) {
+
+                ans[i] = sum;
+
+                // Remove the element leaving the window
+                sum -= code[(i - k + n) % n];
+
+                // Add the new element
+                sum += code[i];
+            }
+        }
+
+        return ans;
+    }
+}
